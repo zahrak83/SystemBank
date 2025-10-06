019a8704c2dc8c1ecf10c70cdadbdc247144a176
Zahra <zaahrra8305@gmail.com>
2025-10-06 22:55:15 +0330

    update repository

--- SystemBank/Infrastructure/Repositories/CardRepository.cs
+++ SystemBank/Infrastructure/Repositories/CardRepository.cs
@@ -5,14 +5,23 @@ namespace SystemBank.Infrastructure.Repositories
 {
     public class CardRepository : ICardRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public CardRepository()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
         public bool CardExist(string cardNumber, string password)
         {
-            throw new NotImplementedException();
+            return _appDbContext.Cards
+                .Any(c => c.CardNumber == cardNumber && c.Password == password);
         }
 
         public bool CardIsActive(string cardNumber)
         {
-            throw new NotImplementedException();
+            return _appDbContext.Cards
+                .Any(c => c.CardNumber == cardNumber && c.IsActive);
         }
 
         public float GetBalance(string cardNumber)
