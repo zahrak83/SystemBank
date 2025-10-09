18c3a8f6deba6b85874fdaf88f4488d66bdf8d65
Zahra <zaahrra8305@gmail.com>
2025-10-09 15:21:03 +0330

    add interface services

--- SystemBank/Interface/IService/ICardService.cs
+++ SystemBank/Interface/IService/ICardService.cs
@@ -1,12 +1,11 @@
-﻿using SystemBank.Entities;
+﻿using SystemBank.Dtos;
+using SystemBank.Entities;
 
 namespace SystemBank.Interface.IService
 {
     public interface ICardService
     {
-        Card Authenticate(string cardNumber, string password);
-        Card Transfer(string sourceCardNumber, string destinationCardNumber, float amount);
-        List<Transaction> GetTransactions(string cardNumber);
+        public Result Login(string cardNumber, string password);
     }
 }
 
---
+++ SystemBank/Interface/IService/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using SystemBank.Dtos;
+
+namespace SystemBank.Interface.IService
+{
+    public interface ITransactionService
+    {
+        List<GetTransactionDto> GetAll(string cardNumber);
+        Result Transfer(string sourceCardNumber, string destinationCardNumber, float amount);
+    }
+}
